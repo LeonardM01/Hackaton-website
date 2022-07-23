7bd5841474351cdb9115b633961b812f86f0298a
Jan Celin <jan.celin03@gmail.com>
2022-07-23 20:50:07 +0200

    Updated Post.js file

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const TruckSchema = new Schema({
+const PostSchema = new Schema({
   title: { type: String, required: true },
   body: { type: String, required: true },
   date: { type: Date, required: true, default: Date.now },
