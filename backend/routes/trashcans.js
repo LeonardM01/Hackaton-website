55808d8bfac279fc5cd65d85668e04c3227c819c
LeonardM01 <martinisleo@gmail.com>
2022-07-23 22:37:40 +0200

    Added map

--- backend/routes/trashcans.js
+++ backend/routes/trashcans.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const trashcanController = require("../controllers/trashcanController.js");
+// const trashcanController = require("../controllers/trashcanController.js");
 
 
-router.post("/createTrashcan", trashcanController.createTrashcan);
-router.get("/getOneTrashcan", trashcanController.getOneTrashcan);
-router.patch("/getAllTrashcans", trashcanController.getAllTrashcans);
+// router.post("/createTrashcan", trashcanController.createTrashcan);
+// router.get("/getOneTrashcan", trashcanController.getOneTrashcan);
+// router.patch("/getAllTrashcans", trashcanController.getAllTrashcans);
 
 module.exports = router;
