7caac4aeea862f1563b9abc90ec524fa24d03315
petra5555 <petra.starcevic@gmail.com>
2022-07-23 14:49:30 +0200

    about

---
+++ frontend/src/components/About/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Navbar, Footer } from '../index.js';
+
+const About = () => {
+  return (
+    <>
+        <Navbar />
+        <Grid container spacing={1}>
+            <Grid item xs={6} md={8}>
+                <img src={`${item.img}?w=164&h=164&fit=crop&auto=format`} />
+            </Grid>
+        </Grid>
+        <Footer />
+    </>
+  )
+}
+
+export default About
