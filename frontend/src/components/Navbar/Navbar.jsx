904991e665677fe7e3a05c4cf2b3ccd93fcc3d63
LeonardM01 <martinisleo@gmail.com>
2022-07-23 13:16:52 +0200

    Finished navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { AppBar, Box, Toolbar, IconButton, Typography, useMediaQuery, Link } from '@mui/material';
+import { React, useState } from 'react';
+import { AppBar, Box, Toolbar, IconButton, Typography, useMediaQuery, Link, Drawer } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
 import useStyles from './styles.js';
@@ -7,6 +7,7 @@ import useStyles from './styles.js';
 function Navbar() {
   const classes = useStyles();
   const isMobile = useMediaQuery('(max-width: 768px)');
+  const [mobileOpen, setMobileOpen] = useState(false);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -19,6 +20,7 @@ function Navbar() {
               color="inherit"
               aria-label="open drawer"
               sx={{ mr: 2 }}
+              onClick={() => setMobileOpen((previousMobileOpen) => !previousMobileOpen)}
             >
               <MenuIcon />
             </IconButton>
@@ -27,12 +29,40 @@ function Navbar() {
             variant="h6"
             noWrap
             component="div"
-            sx={{ flexGrow: 1, disolay: { xs: 'none', sm: 'block' } }}
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
           >
             <Link className={classes.link} to="/">
               Početna
             </Link>
+            <Link className={classes.link} to="/">
+              Novosti
+            </Link>
+            <Link className={classes.link} to="/">
+              Karta
+            </Link>
           </Typography>
+          {isMobile && (
+            <Drawer
+              varianty="temporary"
+              anchor="top"
+              open={mobileOpen}
+              onClose={() => setMobileOpen((previousMobileOpen) => !previousMobileOpen)}
+              classes={{ paper: classes.drawerPaper }}
+            >
+              <Link className={classes.linkDrawer} to="/">
+                Početna
+              </Link>
+              <Link className={classes.linkDrawer} to="/">
+                Novosti
+              </Link>
+              <Link className={classes.linkDrawer} to="/">
+                Karta
+              </Link>
+              <Link className={classes.login} to="/" paddingBottom="1em">
+                Log in
+              </Link>
+            </Drawer>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
