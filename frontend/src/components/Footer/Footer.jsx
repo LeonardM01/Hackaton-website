9763cdde4195a23aecc5dffdf4c8c61f4bbce6ca
petra5555 <petra.starcevic@gmail.com>
2022-07-24 07:22:52 +0200

    footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import { Box, Container, Grid } from '@mui/material';
-import HomeIcon from '@mui/icons-material/Home';
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
-import PersonIcon from '@mui/icons-material/Person';
-import EmailIcon from '@mui/icons-material/Email';
 
 import useStyles from './styles.js';
 
@@ -14,33 +10,33 @@ function Footer() {
     <Box className={classes.footer}>
       <Box
         px={{ xs: 3, sm: 10 }}
-        py={{ xs: 5, sm: 10 }}
+        py={{ xs: 3, sm: 10 }}
       >
         <Container maxWidth="lg">
           <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
             <Grid item xs={3} sm={4} md={3} className={classes.tekst}>
               <h3>Kontakt sa strankama u Dežurnom centru</h3>
-              <LocalPhoneIcon /><span>+385 51 209 497</span><br />
-              <LocalPhoneIcon /><span>0800 51 00</span><br />
-              <EmailIcon /><span>komunalno.dezurstvo@rijeka.hr</span><br />
+              <p>+385 51 209 497</p>
+              <p>0800 51 00</p>
+              <p>komunalno.dezurstvo@rijeka.hr</p>
             </Grid>
             <Grid item xs={3} sm={4} md={3} className={classes.tekst}>
               <h3>Voditeljica službe za komunalno reddarstvo</h3>
-              <LocalPhoneIcon /><span>+385 51 209 487</span><br />
-              <PersonIcon /><span>Jukić Lovrić Jelena</span><br />
-              <EmailIcon /><span>jelena.jukic-lovric@rijeka.hr</span><br />
+              <p>+385 51 209 487</p>
+              <p>Jukić Lovrić Jelena</p>
+              <p>jelena.jukic-lovric@rijeka.hr</p>
             </Grid>
             <Grid item xs={3} sm={4} md={3} className={classes.tekst}>
               <h3>Komunalna redarka</h3>
-              <LocalPhoneIcon /><span>+385 51 209 497</span><br />
-              <PersonIcon /><span>Jurkić Mirjana</span><br />
-              <EmailIcon /><span>mirjana.jurkic@rijeka.hr</span><br />
+              <p>+385 51 209 497</p>
+              <p>Jurkić Mirjana</p>
+              <p>mirjana.jurkic@rijeka.hr</p>
             </Grid>
             <Grid item xs={3} sm={4} md={3} className={classes.tekst}>
               <h3>Kontakt grada Rijeke</h3>
-              <HomeIcon /><span>Korzo 16, 51000 Rijeka</span><br />
-              <LocalPhoneIcon /><span>+385 51 209 334</span><br />
-              <LocalPhoneIcon /><span>faks: +385 51 209 334</span><br />
+              <p>Korzo 16, 51000 Rijeka</p>
+              <p>+385 51 209 334</p>
+              <p><b>faks:</b> +385 51 209 334</p>
             </Grid>
           </Grid>
         </Container>
